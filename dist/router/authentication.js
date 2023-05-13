677102e5cbe679266a01230339d51004bc0ce660
omoniyi22 <omoniyioluwaseun22gmail.com>
2023-05-13 22:55:04 +0100

    removed dist

--- dist/router/authentication.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const authentication_1 = require("./../controllers/authentication");
-exports.default = (router) => {
-    router.post('/auth/register', authentication_1.register);
-    router.post('/auth/login', authentication_1.login);
-};
-//# sourceMappingURL=authentication.js.map
