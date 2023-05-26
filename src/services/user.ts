524056053121741a311664e1f662584b832fa211
omoniyi22 <omoniyioluwaseun22gmail.com>
2023-05-26 04:40:31 +0100

    add handler

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,13 +1,15 @@
-import { UserModel } from "./../db/users";
+import { UserModel } from "./../models/profile";
 
-export const getUsers = () => UserModel.find();
+// USER ACCESS
+
+export const getUserById = (id: string) => UserModel.findById(id)
 export const getUserByEmail = (email: string) => UserModel.findOne({ email })
 export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({
   "authentication.sessionToken": sessionToken
 })
+export const getUsers = () => UserModel.find();
 
-export const getUserById = (id: string) => UserModel.findById(id)
 export const createUser = (values: Record<string, any>) => new UserModel(values).save().then((user) => user.toObject());
 export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id })
 export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
- 
+// ADMIN ACCESS
